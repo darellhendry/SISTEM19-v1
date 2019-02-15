e58e395616ac56dc653f2860abc1ef6c31415927
darell <1darellhendry@gmail.com>
2019-02-15 20:10:36 +0700

    Updates

---
+++ precache-manifest.6ac36eaffac59f4d815100f1e7d9342b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3b60a683e860bbf6cf",
+    "url": "/v1/static/css/main.ff3ce92f.chunk.css"
+  },
+  {
+    "revision": "4c3b60a683e860bbf6cf",
+    "url": "/v1/static/js/main.4c3b60a6.chunk.js"
+  },
+  {
+    "revision": "cc9d5f7bf75d9554a366",
+    "url": "/v1/static/js/runtime~main.cc9d5f7b.js"
+  },
+  {
+    "revision": "e0f8fed5fc641abeb526",
+    "url": "/v1/static/js/2.e0f8fed5.chunk.js"
+  },
+  {
+    "revision": "fe4af5ef1c97d0d0437d71049a62b71a",
+    "url": "/v1/index.html"
+  }
+];
