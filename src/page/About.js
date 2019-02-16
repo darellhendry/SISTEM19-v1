7d02784fd020580001104315deb05b4f55c727b0
darell <1darellhendry@gmail.com>
2019-02-16 21:27:31 +0700

    90%

--- src/page/About.js
+++ src/page/About.js
@@ -51,8 +51,8 @@ const styles = theme => ({
   }
 });
 const description = {
-  College: 'There are some activities that i have done while I am study in college. My first experience is joining a committe team, it\'s Fasilkom Graduation team. Then, while i did in graduation committe, I registered to FUKI, it\'s islamic organazition in Fasilkom and many more. Now i want to focus on my study and try to apply resume to some companies',
-  Movie:'Sed ad unum fabulas, posse copiosae menandri an usu. Sit ex nominavi reformidans. Qui ea omnium corpora, ex nam vivendo copiosae recusabo. An pro eirmod voluptua reprehendunt, ut adipisci praesent usu, no idque tibique luptatum sed.',
+  College: '',
+  Movie:'',
   Code:'Ea utamur aliquam intellegebat mel. In vel impetus delenit expetendis, sea eu minimum copiosae quaestio, eruditi perfecto te quo. Stet soleat concludaturque vis id, autem etiam feugiat te vix. Nihil cetero phaedrum cu nec, wisi altera et nam. Eu verear delenit pri. Dolore vocibus deleniti qui ut, veniam dictas an quo.'
 }
 function About(props) {
@@ -73,7 +73,9 @@ function About(props) {
                   <img className={classes.image} src={process.env.PUBLIC_URL + '/img/fuki-logo.png'} />
                 </Grid>
                 <Grid item>
-                  <Typography variant='body1' style={{textAlign: 'justify'}}>{description['College']}</Typography>
+                  <Typography variant='body1' style={{textAlign: 'justify'}}>
+                    There are some activities that i have done while I am study in college. My first experience is joining a committe team, it's <strong>Fasilkom Graduation</strong>  team. Then, while i did in graduation committe, I registered to <strong>FUKI</strong>, it's islamic organazition in Fasilkom and many more. Now i want to focus on my study and try to apply resume to some companies
+                  </Typography>
                 </Grid>
                 <Grid item>
                   <div className={classes.description}>
@@ -89,9 +91,16 @@ function About(props) {
           <ul className={classes.ul}>
             <ListSubheader className={classes.subheader}><Typography variant='h1' align='center'>Movie</Typography></ListSubheader>
             <ListItem key='Movie' style={{minHeight: 500}}>
-              <span className={classes.description}>
-                <Typography variant='body1' style={{color: '#2D2D2D'}}>{description['Movie']}</Typography>
-              </span>
+              <Grid container justify='center' spacing={16}>
+                <Grid item>
+                  <img className={classes.image} src={process.env.PUBLIC_URL + '/img/mov-logo.png'} />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                  <Typography variant='body1' style={{textAlign: 'justify'}}>
+                    Watching the movie is a simple way to entertain yourself. When I study around 2 hours or more, I get bored. At that time I chose to watching film like anime, korean drama or movie. It's simple to make you entartained, you don't have think to much when you are watching the film, you don't have make a move like playing games hehe
+                  </Typography>
+                </Grid>
+              </Grid>
             </ListItem>
           </ul>
         </li>
@@ -100,9 +109,13 @@ function About(props) {
           <ul className={classes.ul}>
             <ListSubheader className={classes.subheader}><Typography variant='h1' align='center'>Code</Typography></ListSubheader>
             <ListItem key='Code' style={{minHeight: 500}}>
-              <span className={classes.description}>
-                <Typography variant='body1' style={{color: '#2D2D2D'}}>{description['Code']}</Typography>
-              </span>
+              <Grid container justify='center'>
+                <Grid item>
+                  <div className={classes.description}>
+                    <Typography variant='body1' style={{color: '#2D2D2D'}}> “Everybody in this country should learn to program a computer, because it teaches you how to think”<em>-Steve Job</em></Typography>
+                  </div>
+                </Grid>
+              </Grid>
             </ListItem>
           </ul>
         </li>
